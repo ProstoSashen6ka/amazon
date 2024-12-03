73937909ce6d3cec74090fde188886c16db45f58
ProstoSashen6ka <gorodnikalexandr@gmail.com>
2024-12-03 09:38:38 +0300

    Navigation

---
+++ src/app/components/Navigation.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+export default function Navigation(){
+    const pathname = usePathname()
+    return(
+        <nav className="flex bg-zinc-400">
+            <div className="flex-1 w-56 h-12"><Link  className={`${pathname === '/' ? 'text-teal-400': 'hover:text-teal-400'}`} href="/">Home</Link></div>
+            <div className="flex-1 w-56 h-12"><Link  className={`${pathname === '/shopping-cart' ? 'text-teal-400': 'hover:text-teal-400'}`} href="/shopping-cart">Shopping cart</Link></div>
+        </nav>
+    )
+}
